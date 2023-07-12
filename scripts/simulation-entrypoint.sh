f3a7d48159bd2c3f3e6055c7321724ca82f42b75
Li Bai <lbai@temple.edu>
2023-07-12 18:18:19 -0400

    Update simulation-entrypoint.sh

--- scripts/simulation-entrypoint.sh
+++ scripts/simulation-entrypoint.sh
@@ -2,6 +2,7 @@
 source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo-11/setup.sh
 source /home/robomaker/workspace/robot_ws/install/setup.sh
+source /home/robomaker/workspace/simulation_ws/install/setup.sh
 source ./install/setup.sh
 
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(rospack find mini_pupper_simulation)/worlds/meshes
