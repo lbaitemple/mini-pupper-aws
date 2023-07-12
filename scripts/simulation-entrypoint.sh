66406e08c77322b6074477b4548ca0f3c3351e0a
Li Bai <lbai@temple.edu>
2023-07-12 17:02:54 -0400

    Update simulation-entrypoint.sh

--- scripts/simulation-entrypoint.sh
+++ scripts/simulation-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /opt/ros/melodic/setup.bash
+source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo-9/setup.sh
 source /home/robomaker/workspace/robot_ws/install/setup.sh
 source ./install/setup.sh
