fd4c314174a3c978208bf3cd170e0882d73c1016
Li Bai <lbai@temple.edu>
2023-08-31 20:36:50 -0400

    Add files via upload

--- robot_ws/src/mini_pupper_ros/mini_pupper_dance/launch/ndance.launch.py
+++ robot_ws/src/mini_pupper_ros/mini_pupper_dance/launch/ndance.launch.py
@@ -3,6 +3,9 @@ import os
 from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 
 def generate_launch_description():
 
@@ -14,6 +17,10 @@ def generate_launch_description():
         'routines'
     )
 
+    bringup_path = os.path.join(
+        get_package_share_directory('mini_pupper_bringup'),
+        'launch/bringup.launch.py'
+    )
     env = SetEnvironmentVariable(
             'PYTHONPATH',
             os.pathsep.join([dance_config_path, os.environ.get('PYTHONPATH', '')])
@@ -47,8 +54,13 @@ def generate_launch_description():
                 {"dance_config_path": dance_config_path}
             ]
         )    
+
+    launch_bringup=IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(bringup_path)
+        )
     
     ld.add_action(env)
+    ld.add_action(launch_bringup)
     ld.add_action(dance_node)    
     ld.add_action(pose_controller_node)   
     
