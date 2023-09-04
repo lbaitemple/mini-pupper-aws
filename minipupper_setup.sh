f58bb910d543d142bb5b995aa4081d0540d2abc3
Li Bai <lbai@temple.edu>
2023-09-04 15:57:45 -0400

    Update minipupper_setup.sh

--- minipupper_setup.sh
+++ minipupper_setup.sh
@@ -6,5 +6,5 @@ export DEPLOYMENT_BUCKET=mangdang2023
 ##### no need to change anything below
 export DANCE_FILE=demo.py
 cd ~/environment/mini-pupper-aws
-aws s3 cp greengrass/docker-compose.yaml s3://${DEPLOYMENT_BUCKET}/com.example.ros.pupper.dance/1.0.0/artifacts/docker-compose.yaml
+aws s3 cp docker-compose.yaml s3://${DEPLOYMENT_BUCKET}/com.example.ros.pupper.dance/1.0.0/artifacts/docker-compose.yaml
 aws greengrassv2 create-component-version     --inline-recipe fileb://greengrass/com.example.ros.pupper.dance/1.0.0/recipes/recipe.yaml
