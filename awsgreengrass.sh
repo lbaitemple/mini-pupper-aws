15098df83088d974577e02282fd7a0363e51a31e
Li Bai <lbai@temple.edu>
2023-09-06 14:18:46 -0400

    Update awsgreengrass.sh

--- awsgreengrass.sh
+++ awsgreengrass.sh
@@ -6,15 +6,19 @@ export DEPLOYMENT_BUCKET=mangdang2023
 
 export DANCE_FILE=demo.py
 export IOT_ENDPOINT=`aws iot describe-endpoint | grep amazon | cut -d: -f 2 | sed 's/\"//g'` 
-
+export YOUR_PRIVATE_ECR_IMAGE=`aws ecr describe-repositories | grep repositoryUri | grep ros-humble-greengrass-minipupper | cut -d: -f 2 | sed 's/\"//g' | sed 's/\,//g'` | tr -d ' '
+#cd ~/environment/mini-pupper-aws
+export ECR_IMAGE=${YOUR_PRIVATE_ECR_IMAGE}":latest"
+echo $ECR_IMAGE
+export RECIPE_CONFIG_FILE=greengrass/recipe.yaml
 
 ##### no need to change anything below
 aws s3 cp greengrass/docker-compose.yaml s3://${DEPLOYMENT_BUCKET}/artifacts/docker-compose.yaml
 aws s3 sync robot_ws/src/mini_pupper_ros/mini_pupper_dance/routines s3://${DEPLOYMENT_BUCKET}/artifacts/routines
 IOT_CONFIG_FILE=greengrass/aws_iot_params.yaml
 cat ${IOT_CONFIG_FILE}.template | sed -e "s/IOT_ENDPOINT_PLACEHOLDER/${IOT_ENDPOINT}/g" > ${IOT_CONFIG_FILE}
-cd ~/environment/mini-pupper-aws
+
 aws s3 cp greengrass/aws_iot_params.yaml s3://${DEPLOYMENT_BUCKET}/artifacts/aws_iot_params.yaml
-RECIPE_CONFIG_FILE=greengrass/recipe.yaml
-cat ${RECIPE_CONFIG_FILE}.template | sed -e "s/S3_BUCKET_PLACEHOLDER/${DEPLOYMENT_BUCKET}/g"     -e "s/DANCE_ROUTINE_PLACEHOLDER/${DANCE_FILE}/g" > ${RECIPE_CONFIG_FILE}
-aws greengrassv2 create-component-version     --inline-recipe fileb://greengrass/recipe.yaml
+cat ${RECIPE_CONFIG_FILE}.template | sed -e "s#S3_BUCKET_PLACEHOLDER#${DEPLOYMENT_BUCKET}#g"  -e  "s#YOUR_PRIVATE_ECR_IMAGE#${ECR_IMAGE// /}#g"   -e "s#DANCE_ROUTINE_PLACEHOLDER#${DANCE_FILE}#g" > ${RECIPE_CONFIG_FILE}
+aws greengrassv2 create-component-version     --inline-recipe fileb://${RECIPE_CONFIG_FILE}
+
