f081b214cef8e7200f83330939c338e07bcd7566
Li Bai <lbai@temple.edu>
2023-10-15 20:55:30 -0400

    Update flip.sh

--- scripts/flip.sh
+++ scripts/flip.sh
@@ -1,3 +1,3 @@
 source /opt/ros/humble/setup.bash
 
-rosservice call /gazebo/set_model_state '{model_state: { model_name: "/", pose: { position: {x: 0, y: 2.7, z: 0.55}, orientation: {x: 0, y: 0, z: -1.55, w: 1} }, twist: { linear: {x: 0 , y: 0 ,z: 0} , angular: {x: 0 , y: 0 , z: 0} } , reference_frame: world } }'
+ros2 service call /gazebo/set_model_state '{model_state: { model_name: "/", pose: { position: {x: 0, y: 2.7, z: 0.55}, orientation: {x: 0, y: 0, z: -1.55, w: 1} }, twist: { linear: {x: 0 , y: 0 ,z: 0} , angular: {x: 0 , y: 0 , z: 0} } , reference_frame: world } }'
