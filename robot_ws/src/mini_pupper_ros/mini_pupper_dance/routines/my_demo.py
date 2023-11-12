f3ad60073845587e7e235ab15cd871b0b108f7dd
Li Bai <lbai@temple.edu>
2023-11-11 19:37:43 -0500

    Add files via upload

--- robot_ws/src/mini_pupper_ros/mini_pupper_dance/routines/my_demo.py
+++ robot_ws/src/mini_pupper_ros/mini_pupper_dance/routines/my_demo.py
@@ -16,7 +16,7 @@
 # stay: the robot will keep the last command
 dance_commands = [
 'volume:on:100',    
-'music:on:robot1.wav',
+'music:on:how.wav',
 'move_forward:0.5:0.5',
 'stop:0:0.5',
 'look_down:0.3:0.5',
@@ -45,7 +45,7 @@ dance_commands = [
 'look_down:0.3:0.5',
 'look_right:-0.3:0.5',
 
-'music:on:how.wav',    
+'music:on:robot1.wav',    
 'look_down:0.3:0.5',
 'look_left:0.3:0.5',
 'look_up:-0.3:0.5',
