f83394a4a406abdf9619042d4fe78269615c4a0c
Li Bai <lbai@temple.edu>
2024-04-01 08:41:22 -0400

    Update move.sh

--- scripts/move.sh
+++ scripts/move.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 #ros2 run teleop_twist_keyboard teleop_twist_keyboard
-ros2 topic pub -1 /dance_config std_msgs/msg/String data:\ \'demo\'\ 
+ros2 topic pub -1 /dance_config std_msgs/msg/String data:\ \'$1\'\ 
