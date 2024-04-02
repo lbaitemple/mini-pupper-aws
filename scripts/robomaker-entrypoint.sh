39c41774881cb7873c4cb838b0aab6d1d744dddb
Li Bai <lbai@temple.edu>
2024-04-01 20:36:53 -0400

    Update robomaker-entrypoint.sh

--- scripts/robomaker-entrypoint.sh
+++ scripts/robomaker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-source /opt/ros_demos/setup.bash
+#source /opt/ros_demos/setup.bash
 #source /opt/greengress_bridge/setup.bash
 source /home/robomaker/workspace/robot_ws/install/setup.sh
-source /opt/greengrass_bridge/setup.bash
+#source /opt/greengrass_bridge/setup.bash
 source /opt/ros/humble/setup.bash
 
 export PYTHONPATH=$PYTHONPATH:$DANCE_CONFIG:/usr/local/lib/python3.10/dist-packages
